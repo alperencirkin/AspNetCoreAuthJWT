450b328471e98fb88de39dfa45362ea31349d4f7
Alperen Çirkin <alpcirkin@gmail.com>
2021-08-26 01:43:30 +0300

    Update UsersController.cs

--- AspNetCoreAuthJWT/Controllers/UsersController.cs
+++ AspNetCoreAuthJWT/Controllers/UsersController.cs
@@ -15,6 +15,7 @@ namespace AspNetCoreJWT.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
+
         public UsersController(IUserService userService) => _userService = userService;
 
         [AllowAnonymous]
